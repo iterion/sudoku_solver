faa492c418b95fb5405785ccdeb9392f2adfdf97
Adam Sunderland <adamsunderland@thinishd830.tiu11.org>
2009-10-28 15:17:58 -0400

    Updating Test Bench Added Hard Puzzles - all fail

--- test_bench.rb
+++ test_bench.rb
@@ -5,19 +5,24 @@ s = SudokuSolver.new(true)
 
 times = Array.new
 fail = Array.new
+succeed = Array.new
 file = File.new("puzzles/subig20", "r")
 i = 0
 while (line = file.gets)
-  s.from_file(line)
-
+  if s.from_file(line)
     times << s.solve
-
-  fail << i if s.solved_num < 81
+    if s.solved_num < 81
+      fail << i 
+    else
+      succeed << i
+    end
+  end
   i += 1
   break if i > 500
 end
  
  
 puts "#{times.length} iterations took #{times.sum} seconds (mean: #{times.mean}, median: #{times.median}, std-dev: #{times.deviation})."
-puts fail
+puts fail.join(",")
 puts fail.length.to_f/times.length.to_f
+puts succeed.join(",")
